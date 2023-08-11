54bf10233a8b6f91a455b687a3f32245832de66c
Gabriel447 <fullstackgabe@gmail.com>
2023-08-10 21:03:18 -0300

    implementando js no menu toggle

--- src/views/templates/footer.php
+++ src/views/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <span>@<span class="text-danger">gabee</span>cwb</span>
 </footer>
-<script src=""></script>
+<script src="../../public/assets/js/app.js"></script>
 </body>
 </html>
